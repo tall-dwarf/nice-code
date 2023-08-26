f60ea3ad19e4455e34e95e54ea03bdbca548c1dd
imalofeev6 <imalofeev6@gmail.com>
2023-08-26 18:21:59 +0300

    setup react

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "none",
+    entry: {
+        app: path.join(__dirname, "src", "index.tsx"),
+    },
+    target: "web",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+        rules: [{
+                test: /\.m?jsx$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env", ["@babel/preset-react", { runtime: "automatic" }],
+                        ],
+                    },
+                },
+            },
+            { test: /\.tsx?$/, use: "ts-loader" },
+        ],
+    },
+    optimization: {
+        nodeEnv: "development",
+    },
+
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "index.html"),
+        }),
+    ],
+    devServer: {
+        static: "./dist",
+    },
+};
